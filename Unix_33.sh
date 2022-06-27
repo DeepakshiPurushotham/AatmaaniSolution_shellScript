f6458a920f2ff19fc664072f8fc3bf620da26d91
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-06-27 09:46:04 +0530

    Update Unix_33.sh

--- Unix_33.sh
+++ Unix_33.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Script to Delete Old Files older than 3 days
-read -p "enter file which needs to be delete:" fileName 
-echo `find . -name "$fileName" -type f -mtime +3 -delete`  
+#read -p "enter file which needs to be delete:" fileName 
+echo `find . -type f -mtime +3 -delete`  
