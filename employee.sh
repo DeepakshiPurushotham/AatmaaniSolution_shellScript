46cd3c7c68a4f2d0fa0577932d916629ee6b3b2b
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-06-29 13:10:14 +0530

    Update employee.sh

--- employee.sh
+++ employee.sh
@@ -6,9 +6,9 @@ case $1 in
 
 new)
 
-read -p "enter the string to add database:" st
+#read -p "enter the string to add database:" st
 
-echo `find . -name $fileName | echo "$st" >> $fileName`
+echo `find . -name $fileName | echo "$2, $3, $4, $5" >> $fileName`
 echo "line updated"
 ;;
 
@@ -18,24 +18,24 @@ list)
  ;;
 remove)
 
-read -p "enter the line num which needs to be removed:" n 
-echo `cat $fileName | grep $n | sed -i $n'd' $fileName`
-echo "removed line no. $n"
+#read -p "enter the name which needs to be removed:" n
+echo `sed -i /$2/'d' $fileName`
+echo "removed line no. $2"
 ;;
 
-#clear)
+clear)
 
-#read -p "enter which colum should be removed:" c 
-#echo `awk '{print $3 }'| sed -i  `
-#;;
+#read -p "enter which colum should be removed:" c
+echo `find . -name $fileName | echo "" > $fileName`
+;;
 
 lookup)
-read -p "enter the name:" name
-a=`cat $fileName | grep $name | awk '{print $3}'`
+#read -p "enter the name:" name
+a=`cat $fileName | grep $2 | awk '{print $3}'`
 echo $a
-echo "the ph.no for $name is:" $a
+echo "the ph.no for $2 is:" $a
 ;;
-
+                     
 *)
 echo "usage $0 | new | list  | remove | clear | lookup"
 ;;
