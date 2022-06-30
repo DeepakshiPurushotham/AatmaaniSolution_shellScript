6529a078d8bc72a4556ec98109703cf339722242
DeepakshiPurushotham <deepakshipurushotham@gmail.com>
2022-06-30 07:01:06 +0000

    smpt

--- employee.sh
+++ employee.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-read -p "enter the file name:" fileName
+#read -p "enter the file name:" fileName
 
 case $1 in
 
 new)
 
-read -p "enter the string to add database:" st
+#read -p "enter the string to add database:" st
 
-echo `find . -name $fileName | echo "$st" >> $fileName`
+echo `find . -name employee.txt | echo "$2, $3, $4, $5" >> employee.txt`
 echo "line updated"
 ;;
 
 list)
 #list all the contents with cronological order
- cat $fileName | sort $fileName
+ cat employee.txt | sort employee.txt
  ;;
 remove)
 
-read -p "enter the line num which needs to be removed:" n 
-echo `cat $fileName | grep $n | sed -i $n'd' $fileName`
+#read -p "enter the name which needs to be removed:" n 
+echo `sed -i /$2/'d' employee.txt`
 echo "removed line no. $n"
 ;;
 
-#clear)
+clear)
 
 #read -p "enter which colum should be removed:" c 
-#echo `awk '{print $3 }'| sed -i  `
-#;;
+echo `find . -name employee.txt | echo "" > employee.txt`
+;;
 
 lookup)
-read -p "enter the name:" name
-a=`cat $fileName | grep $name | awk '{print $3}'`
+#read -p "enter the name:" name
+a=`cat employee.txt | grep $2 | awk '{print $3}'`
 echo $a
-echo "the ph.no for $name is:" $a
+echo "the ph.no for $2 is:" $a
 ;;
 
 *)
--- names.sh
+++ names.sh
@@ -1,9 +1,5 @@
-#Name           Age
-Toni            25
-Deepakshi       22
-Jayesh          40
-Parmar          35
-Courtney        36
-Thomas          35
-addition
 
+Deepakshi       22              123456789       HOI
+Purushotham     50              234567891       VOE
+Abigail         29              345678912       VVOE
+Jayesh          35      56789613        Income
