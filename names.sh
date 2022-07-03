6b9c963ca18f3d8c969f949e4213ffa0d4343a73
DeepakshiPurushotham <deepakshipurushotham@gmail.com>
2022-07-03 07:56:58 +0000

    counting the occurance of uniq word

--- names.sh
+++ names.sh
@@ -3,3 +3,12 @@ Deepakshi       22              123456789       HOI
 Purushotham     50              234567891       VOE
 Abigail         29              345678912       VVOE
 Jayesh          35      56789613        Income
+Deepakshi       22              123456789       HOI
+Purushotham     50              234567891       VOE
+Abigail         29              345678912       VVOE
+Jayesh          35      56789613        Income
+Deepakshi       22              123456789       HOI
+Purushotham     50              234567891       VOE
+Abigail         29              345678912       VVOE
+Jayesh          35      56789613        Income
+
